c63fddd7f271399e5f2d9e6709f9aed070ca68f6
icaro <ibsc013@gmail.com>
2022-12-04 12:36:58 -0300

    Operações dos vetores

--- app/src/main/java/com/montaigne/estabilidade_io/Models.kt
+++ app/src/main/java/com/montaigne/estabilidade_io/Models.kt
@@ -1,6 +1,7 @@
 package com.montaigne.estabilidade_io
 
 import java.util.InvalidPropertiesFormatException
+import kotlin.math.sqrt
 
 object Consts {
     @JvmField
@@ -11,8 +12,19 @@ object Consts {
 
 data class Vector(val x: Float, val y: Float) {
     operator fun plus(other: Vector) = Vector(this.x + other.x, this.y + other.y)
-
     operator fun minus(other: Vector) = Vector(this.x - other.x, this.y - other.y)
+    operator fun times(other: Float) = Vector(x * other, y * other)
+    operator fun div(other: Float) = Vector(x / other, y / other)
+
+    fun modulus() = sqrt(this*this)  // = sqrt(x**2 + y**2)
+
+    fun normalize() = this/modulus()
+
+    operator fun times(other: Vector) = this.x * other.x + this.y * other.y  // dot product
+
+    fun crossModule(other: Vector) = this.x * other.x - this.y * other.y
+
+    fun getNormal() = Vector(-y, x).normalize()
 }
 
 
@@ -29,11 +41,12 @@ data class Bar(val knots: List<Knot>) {
 data class Load(val knot: Knot, val vector: Vector)
 
 // forças normais ao comprimento da carga
-data class DistributedLoad(val knots: List<Knot>, val module: Int) {
-    init {if (knots.size != 2)
-            throw InvalidPropertiesFormatException("Uma barra deve possuir dois nós")}
-
-    fun getEqLoad(): Load = TODO()
+data class DistributedLoad(val knot1: Knot, val knot2: Knot, val norm: Float) {
+    fun getEqLoad() = Load(
+        Knot(knot1.name + knot2.name, (knot1.pos + knot2.pos) / 2F),  // midpoint
+        (knot2.pos - knot1.pos).getNormal() * (knot2.pos - knot1.pos).modulus() * norm
+        // bisector vector times modulus of the entire distributed force
+    )
 }
 
 
