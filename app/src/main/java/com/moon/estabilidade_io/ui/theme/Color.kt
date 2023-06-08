068efeadd43ddeb5a89f2add11daf3c076743dc2
Ícaro Bruno <ibsc013@gmail.com>
2023-06-08 18:20:22 -0300

    Initial commit

---
+++ app/src/main/java/com/moon/estabilidade_io/ui/theme/Color.kt
@@ -0,0 +1,37 @@
+package com.moon.estabilidade_io.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+/* Light
+--primary-100:#FF4D4D;
+--primary-200:#ff8279;
+--primary-300:#ffecda;
+--accent-100:#FFA07A;
+--accent-200:#944424;
+--text-100:#333333;
+--text-200:#5c5c5c;
+--bg-100:#FFFFFF;
+--bg-200:#f5f5f5;
+--bg-300:#cccccc;
+ */
+
+/* Dark
+--primary-100:#FF4136;
+--primary-200:#ff7862;
+--primary-300:#ffe2c1;
+--accent-100:#FF851B;
+--accent-200:#8e2800;
+--text-100:#FFFFFF;
+--text-200:#e0e0e0;
+--bg-100:#1E1E1E;
+--bg-200:#2d2d2d;
+--bg-300:#454545;
+ */
